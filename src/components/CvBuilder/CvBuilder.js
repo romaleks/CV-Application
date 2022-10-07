a1658d61b461c6dcf0dc792e45adae1b3633163d
IDDna <romanov1371@gmail.com>
2022-10-07 19:30:18 +0300

    Create basic file structure for CvBuilder

---
+++ src/components/CvBuilder/CvBuilder.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export class CvBuilder extends Component {
+  render() {
+    return <div></div>
+  }
+}
+
+export default CvBuilder
---
+++ src/components/CvBuilder/GeneralInfo.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export class GeneralInfo extends Component {
+  render() {
+    return <div></div>
+  }
+}
+
+export default GeneralInfo
