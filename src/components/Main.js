b778f808dcd26e145db391b00fd9466caf579303
IDDna <romanov1371@gmail.com>
2022-10-14 21:06:43 +0300

    Style Cv preview

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import CvBuilder from './CvBuilder/CvBuilder'
+import CvPreview from './CvPreview/CvPreview'
 
 class Main extends Component {
   render() {
     return (
       <div className='main'>
         <CvBuilder />
+        <CvPreview />
       </div>
     )
   }
