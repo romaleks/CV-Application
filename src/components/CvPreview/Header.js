8f276487ee8fb1f24fa824e513ea2ef26e38ad71
IDDna <romanov1371@gmail.com>
2022-10-14 21:06:16 +0300

    Structure header of preview

--- src/components/CvPreview/Header.js
+++ src/components/CvPreview/Header.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react'
 
 export class Header extends Component {
   render() {
-    return <div className='cv-header'>Header</div>
+    return (
+      <div className='cv-header'>
+        <div className='photo'></div>
+        <div className='names'>
+          <div className='name'>
+            {this.props.firstName}
+            {this.props.LastName}Alexandr Romanov
+          </div>
+          <div className='prof-title'>Web Developer</div>
+        </div>
+      </div>
+    )
   }
 }
 
